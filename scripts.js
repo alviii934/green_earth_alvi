d3febd92e21bad4b3b333c6b5ff9ad47031560e0
Alvi <hasan2305101696@diu.edu.bd>
2026-02-12 19:39:23 +0600

    Modal Functionality fully working with selected card by catagory or even without selected catagory

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// load Catagory
+// load Catagory to aside
 const loadCategory = () => {
   fetch('https://openapi.programming-hero.com/api/categories')
     .then(res => res.json())
@@ -8,7 +8,7 @@ const loadCategory = () => {
 };
 loadCategory();
 
-// Display Catagory to aside 
+// Display Catagory to aside
 const displayLoadCatagory = catagories => {
   // step-1 : Get the container & empty
   const catagoryContainer = document.getElementById('catagory-container');
@@ -18,8 +18,8 @@ const displayLoadCatagory = catagories => {
     // step-3 : crete element
     const btnDiv = document.createElement('div');
     btnDiv.innerHTML = ` 
-               <ul onclick="showCatagoricalTree(${catagory.id})"
-              class="space-y-2 text-sm bg-[#DCFCE7]">
+               <ul id="catagory-btn-${catagory.id}" onclick="showCatagoricalTree(${catagory.id})"
+              class="space-y-2 text-sm bg-[#DCFCE7] common-btn">
              <li class="px-3 py-2 rounded-md hover:bg-[#15803D] cursor-pointer transition">
              ${catagory.category_name}
               </li>
@@ -30,35 +30,43 @@ const displayLoadCatagory = catagories => {
   }
 };
 
+const removeActive = () => {
+  const caAllBtn = document.querySelectorAll('.common-btn');
+  //console.log(caAllBtn)
+  caAllBtn.forEach(btn => btn.classList.remove('active'));
+};
 
 // https://openapi.programming-hero.com/api/category/${id}
 //onClick(showSpecificTree()
 
-const showCatagoricalTree = (id) => {
- fetch(`https://openapi.programming-hero.com/api/category/${id}`)
-   .then(res => res.json())
-   .then(data => {
-     displayCatagoricalTree(data.plants);
-   });
-}
+const showCatagoricalTree = id => {
+  fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      removeActive(); // remove all active class
+      const clickCtaButton = document.getElementById(`catagory-btn-${id}`);
+      //console.log(clickCtaButton);
+      clickCtaButton.classList.add('active');
+      displayCatagoricalTree(data.plants);
+    });
+};
 
-const displayCatagoricalTree = (catWords) => {
-
-
+const displayCatagoricalTree = catWords => {
   const allCatPlantsContainer = document.getElementById('all-plants');
   allCatPlantsContainer.innerHTML = '';
   // step-2 : Get into every lessons
   for (const catWord of catWords) {
     // step-3 : crete element
     const allCatPlantDiv = document.createElement('div');
+   
     allCatPlantDiv.innerHTML = ` 
                              <div class="bg-white rounded-xl shadow-sm hover:shadow-md transition duration-300 w-full">
                   <figure class="p-3">
                     <img class="rounded-md h-36 w-full object-cover" src="${catWord.image}" alt="" />
                   </figure>
 
-                  <div class="p-4 space-y-2">
-                    <h2 class="text-sm font-semibold">${catWord.name}</h2>
+                  <div class="p-4 space-y-2 cursor-pointer transition ">
+                    <h2 onclick="showWordDetailforModal(${catWord.id})"class="text-sm font-semibold">${catWord.name}</h2>
 
                     <p class="text-xs text-gray-500">
                       ${catWord.description}
@@ -84,11 +92,9 @@ const displayCatagoricalTree = (catWords) => {
   //console.log(catWords);
   // get the container empty
   // get into every lesson
-  // crete elemet 
+  // crete elemet
   // append
-}
-
-
+};
 
 // load plants
 const loadPlants = () => {
@@ -100,7 +106,7 @@ const loadPlants = () => {
 };
 loadPlants();
 
-// Display Catagory to aside 
+// Display Catagory to aside
 const displayLoaAllPlants = allPlants => {
   // step-1 : Get the container & empty
   const allPlantsContainer = document.getElementById('all-plants');
@@ -117,8 +123,8 @@ const displayLoaAllPlants = allPlants => {
                     <img class="rounded-md h-36 w-full object-cover" src="${plant.image}" alt="" />
                   </figure>
 
-                  <div class="p-4 space-y-2">
-                    <h2 class="text-sm font-semibold">${plant.name}</h2>
+                  <div class="p-4 space-y-2 cursor-pointer transition">
+                    <h2 onclick="showWordDetailforModal(${plant.id})" class="text-sm font-semibold">${plant.name}</h2>
 
                     <p class="text-xs text-gray-500">
                       ${plant.description}
@@ -143,3 +149,21 @@ const displayLoaAllPlants = allPlants => {
   }
 };
 
+// Modal Part
+const showWordDetailforModal = async id => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  //console.log(url)
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetailforModal(details.plants);
+};
+const displayWordDetailforModal = details => {
+  console.log(details);
+  const detailsModal = document.getElementById('modal-container');
+  detailsModal.innerHTML = `<h1 class="font-bold text-xl">${details.name}</h1>
+        <img class="rounded-md h-56 w-full object-cover" src="${details.image}" alt="">
+        <h2><span class="font-bold">Catagory:</span>${details.category} </h2>
+        <p><span class="font-bold">Price:</span>${details.price}</p>
+        <P> <span class="font-bold">Description:</span>${details.description}</P>`;
+   document.getElementById('modal-design').showModal();
+};
