5907da1bc805801a41df18a7336ff6d1891a0daa
Alvi <hasan2305101696@diu.edu.bd>
2026-02-12 13:11:19 +0600

    Catagorical tree implementation by clicking catagory buttons

--- scripts.js
+++ scripts.js
@@ -18,16 +18,76 @@ const displayLoadCatagory = catagories => {
     // step-3 : crete element
     const btnDiv = document.createElement('div');
     btnDiv.innerHTML = ` 
-              <ul class="space-y-2 text-sm bg-[#DCFCE7]">
-                <li class="px-3 py-2 rounded-md hover:bg-[#15803D] cursor-pointer transition">
-                  ${catagory.category_name}
-                </li>
+               <ul onclick="showCatagoricalTree(${catagory.id})"
+              class="space-y-2 text-sm bg-[#DCFCE7]">
+             <li class="px-3 py-2 rounded-md hover:bg-[#15803D] cursor-pointer transition">
+             ${catagory.category_name}
+              </li>
               </ul>
               `;
     // step-4 : append into container
     catagoryContainer.append(btnDiv);
   }
 };
+
+
+// https://openapi.programming-hero.com/api/category/${id}
+//onClick(showSpecificTree()
+
+const showCatagoricalTree = (id) => {
+ fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+   .then(res => res.json())
+   .then(data => {
+     displayCatagoricalTree(data.plants);
+   });
+}
+
+const displayCatagoricalTree = (catWords) => {
+
+
+  const allCatPlantsContainer = document.getElementById('all-plants');
+  allCatPlantsContainer.innerHTML = '';
+  // step-2 : Get into every lessons
+  for (const catWord of catWords) {
+    // step-3 : crete element
+    const allCatPlantDiv = document.createElement('div');
+    allCatPlantDiv.innerHTML = ` 
+                             <div class="bg-white rounded-xl shadow-sm hover:shadow-md transition duration-300 w-full">
+                  <figure class="p-3">
+                    <img class="rounded-md h-36 w-full object-cover" src="${catWord.image}" alt="" />
+                  </figure>
+
+                  <div class="p-4 space-y-2">
+                    <h2 class="text-sm font-semibold">${catWord.name}</h2>
+
+                    <p class="text-xs text-gray-500">
+                      ${catWord.description}
+                    </p>
+
+                    <div class="flex justify-between items-center">
+                      <span onClick() class="text-xs bg-[#CFF0DC] text-[#15803D] px-2 py-1 rounded-full">
+                        ${catWord.category}
+                      </span>
+                      <span class="font-semibold text-sm">৳<span> ${catWord.price}</span></span>
+                    </div>
+
+                    <button
+                      class="w-full mt-2 text-sm py-2 rounded-full bg-[#15803D] text-white hover:bg-green-700 cursor-pointer transition">
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              `;
+    // step-4 : append into container
+    allCatPlantsContainer.append(allCatPlantDiv);
+  }
+  //console.log(catWords);
+  // get the container empty
+  // get into every lesson
+  // crete elemet 
+  // append
+}
+
 
 
 // load plants
@@ -65,14 +125,14 @@ const displayLoaAllPlants = allPlants => {
                     </p>
 
                     <div class="flex justify-between items-center">
-                      <span class="text-xs bg-[#CFF0DC] text-[#15803D] px-2 py-1 rounded-full">
+                      <span onClick() class="text-xs bg-[#CFF0DC] text-[#15803D] px-2 py-1 rounded-full">
                         ${plant.category}
                       </span>
                       <span class="font-semibold text-sm">৳<span> ${plant.price}</span></span>
                     </div>
 
                     <button
-                      class="w-full mt-2 text-sm py-2 rounded-full bg-[#15803D] text-white hover:bg-green-700 transition">
+                      class="w-full mt-2 text-sm py-2 rounded-full bg-[#15803D] text-white hover:bg-green-700 cursor-pointer transition">
                       Add to Cart
                     </button>
                   </div>
